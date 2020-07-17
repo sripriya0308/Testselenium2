eff7cf62ded4f0fcdebc7d493eab691623bb09b9
sripriya0308 <sripriya0308@gmail.com>
2020-07-17 12:32:12 +0530

    My first commit

---
+++ src/test/java/org/test/project/ProjectTest.java
@@ -0,0 +1,12 @@
+package org.test.project;
+
+public class ProjectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing");
+
+	}
+
+}
